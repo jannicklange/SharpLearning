26b7c19f7341f88ad76a37b6d07c3cddcfe43d04
Jannick.Lange <jannick.lange@orconomy.de>
2017-05-15 19:05:02 +0200

    refs #18794: added stubs for custom split criterion + impurity calculation

---
+++ src/SharpLearning.DecisionTrees/ImpurityCalculators/TopPerformanceImpurityCalculator.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLearning.DecisionTrees.ImpurityCalculators
+{
+    using SharpLearning.Containers.Views;
+
+    /// <summary>
+    /// Computes a custom split criterion. Goal is to separate the top performing q% from the remaining nodes.
+    /// Should be used together with <see cref="TopPerformanceSplitSearcher"/>.
+    /// </summary>
+    public class TopPerformanceImpurityCalculator : IImpurityCalculator
+    {
+        Interval1D m_interval;
+        int m_currentPosition;
+
+        double m_weightedTotal = 0.0;
+        double m_weightedLeft = 0.0;
+        double m_weightedRight = 0.0;
+
+        double m_meanLeft = 0.0;
+        double m_meanRight = 0.0;
+        double m_meanTotal = 0.0;
+
+        double m_sqSumLeft = 0.0;
+        double m_sqSumRight = 0.0;
+        double m_sqSumTotal = 0.0;
+
+        double m_varLeft = 0.0;
+        double m_varRight = 0.0;
+
+        double m_sumLeft = 0.0;
+        double m_sumRight = 0.0;
+        double m_sumTotal = 0.0;
+
+        double[] m_targets;
+        double[] m_weights;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double WeightedLeft { get { return m_weightedLeft; } }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double WeightedRight { get { return m_weightedRight; } }
+
+        public void Init(double[] uniqueTargets, double[] targets, double[] weights, Interval1D interval)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateInterval(Interval1D newInterval)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateIndex(int newPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            m_currentPosition = m_interval.FromInclusive;
+
+            m_weightedLeft = 0.0;
+            m_weightedRight = m_weightedTotal;
+
+            m_meanRight = m_meanTotal;
+            m_meanLeft = 0.0;
+            m_sumRight = m_sqSumTotal;
+            m_sqSumLeft = 0.0;
+
+            m_varRight = (m_sqSumRight / m_weightedTotal -
+                m_meanRight * m_meanRight);
+            m_varLeft = 0.0;
+
+            m_sumRight = m_sumTotal;
+            m_sumLeft = 0.0;
+        }
+
+        public double ImpurityImprovement(double impurity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double NodeImpurity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ChildImpurities ChildImpurities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double LeafValue()
+        {
+            return m_meanTotal;
+        }
+
+        public double[] TargetNames
+        {
+            get { return new double[0]; }
+        }
+
+        public double[] LeafProbabilities()
+        {
+            return new double[0];
+        }
+    }
+}
---
+++ src/SharpLearning.DecisionTrees/SplitSearchers/TopPerformanceResolutionSplitSearcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLearning.DecisionTrees.SplitSearchers
+{
+    using SharpLearning.Containers.Views;
+    using SharpLearning.DecisionTrees.ImpurityCalculators;
+
+    public class TopPerformanceResolutionSplitSearcher : ISplitSearcher
+    {
+        /// <summary>
+        /// TODO The find best split.
+        /// </summary>
+        /// <param name="impurityCalculator">
+        /// TODO The impurity calculator.
+        /// </param>
+        /// <param name="feature">
+        /// TODO The feature.
+        /// </param>
+        /// <param name="targets">
+        /// TODO The targets.
+        /// </param>
+        /// <param name="parentInterval">
+        /// TODO The parent interval.
+        /// </param>
+        /// <param name="parentImpurity">
+        /// TODO The parent impurity.
+        /// </param>
+        /// <returns>
+        /// The <see cref="SplitResult"/>.
+        /// </returns>
+        public SplitResult FindBestSplit(IImpurityCalculator impurityCalculator, double[] feature, double[] targets, Interval1D parentInterval, double parentImpurity)
+        {
+            var result = new SplitResult();
+            return result;
+        }
+    }
+}
